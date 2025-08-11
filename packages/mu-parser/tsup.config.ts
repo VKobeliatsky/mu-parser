519ccb675fea63cbb70c020bc1b1ecb2d00c1736
VKobeliatsky <vladimir.kobeliatsky@gmail.com>
2025-08-11 23:01:54 +0200

    Initial commit

---
+++ packages/mu-parser/tsup.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig([
+  // CommonJS build
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    dts: true,
+    clean: true,
+    minify: true,
+    platform: 'node',
+    outDir: 'dist/cjs',
+    target: 'es2018',
+    sourcemap: true,
+  },
+  // ESM build
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    minify: true,
+    platform: 'neutral',
+    outDir: 'dist/esm',
+    target: 'es2018',
+    sourcemap: true,
+  },
+  // Browser IIFE build
+  {
+    entry: ['src/index.ts'],
+    format: ['iife'],
+    globalName: 'MuParser',
+    minify: true,
+    platform: 'browser',
+    outDir: 'dist/browser',
+    target: 'es2018',
+    sourcemap: true,
+  },
+])
